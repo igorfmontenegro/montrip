bd51ea85840a250cba1618dbcff6e6b8d30a61ff
igorfmontenegro <igor.muntenegro@icloud.com>
2024-02-07 22:21:01 -0300

    feat: add menu mobile

--- src/components/Header/Header.styles.ts
+++ src/components/Header/Header.styles.ts
@@ -6,6 +6,10 @@ interface ContainerProps {
   scrolled: boolean | string
 }
 
+interface MenuMobileProps {
+  open: boolean
+}
+
 export const ImgLogo = styled.img`
   width: 100px;
 `
@@ -16,7 +20,7 @@ export const Container = styled.div<ContainerProps>`
   position: fixed;
   top: 0;
   left: 0;
-  z-index: 500;
+  z-index: 997;
 
   a {
     color: #fff;
@@ -26,6 +30,10 @@ export const Container = styled.div<ContainerProps>`
 
   ${ImgLogo} {
     content: url(${Logo});
+
+    &:hover {
+      transform: scale(1.05);
+    }
   }
 
   ${(props) =>
@@ -52,10 +60,6 @@ export const Content = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-
-  a:hover {
-    transform: scale(1.05);
-  }
 `
 
 export const NavMenu = styled.nav`
@@ -72,6 +76,17 @@ export const NavMenu = styled.nav`
   }
 `
 
+export const MenuDesktop = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  width: 100%;
+
+  @media (max-width: 964px) {
+    display: none;
+  }
+`
+
 export const Button = styled.button`
   padding: 10px 25px;
   font-family: 'Poppins', sans-serif;
@@ -79,4 +94,69 @@ export const Button = styled.button`
   border: none;
   color: #fff;
   cursor: pointer;
+`
+
+export const ContainerSidebar = styled.div<ContainerProps>`
+  display: none;
+  svg {
+    color: ${(props) => (props.scrolled ? '#5f8dc9' : '#fff')};
+    font-size: 40px;
+  }
+
+  @media (max-width: 964px) {
+    display: block;
+  }
+`
+
+export const OverlayMenu = styled.div<MenuMobileProps>`
+  background-color: rgba(0, 0, 0, 0.7);
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 998;
+  display: ${(props) => (props.open ? 'block' : 'none')};
+`
+
+export const MenuMobile = styled.div<MenuMobileProps>`
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  right: 0;
+  background-color: #fff;
+  z-index: 999;
+  width: ${(props) => (props.open ? '70%' : '0%')};
+  overflow: hidden;
+  transition: 0.2s;
+
+  ul {
+    text-align: right;
+    display: flex;
+    flex-direction: column;
+    line-height: 50px;
+    margin-top: 80px;
+  }
+
+  li {
+    margin: 0;
+  }
+
+  a {
+    color: #000;
+    font-size: 1.2em;
+    display: block;
+    padding: 20px 8%;
+
+    &:hover {
+      background: #5f8dc9;
+      color: #fff;
+    }
+  }
+
+  svg {
+    color: #5f8dc9;
+    font-size: 60px;
+    margin: 20px 4%;
+  }
 `
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,22 @@
 import { useEffect, useState } from 'react'
-import { Button, Container, Content, ImgLogo, NavMenu } from './Header.styles'
+import {
+  Button,
+  Container,
+  ContainerSidebar,
+  Content,
+  ImgLogo,
+  MenuDesktop,
+  MenuMobile,
+  NavMenu,
+  OverlayMenu
+} from './Header.styles'
+
+import { GrFormClose } from 'react-icons/gr'
+import { FaBars } from 'react-icons/fa'
 
 export function Header() {
   const [scrolled, setScrolled] = useState(false)
+  const [menuActive, setMenuActive] = useState(false)
 
   useEffect(() => {
     function handleScroll() {
@@ -28,28 +42,68 @@ export function Header() {
           <ImgLogo />
         </a>
 
-        <NavMenu>
-          <ul>
-            <li>
-              <a href="#home"> Home </a>
-            </li>
-            <li>
-              <a href="#about"> Sobre </a>
-            </li>
-            <li>
-              <a href="#budgets"> Orçamentos </a>
-            </li>
-            <li>
-              <a href="#destiny"> Destinos </a>
-            </li>
-          </ul>
-        </NavMenu>
+        <MenuDesktop>
+          <NavMenu>
+            <ul>
+              <li>
+                <a href="#home"> Home </a>
+              </li>
+              <li>
+                <a href="#about"> Sobre </a>
+              </li>
+              <li>
+                <a href="#budgets"> Orçamentos </a>
+              </li>
+              <li>
+                <a href="#destiny"> Destinos </a>
+              </li>
+            </ul>
+          </NavMenu>
+          <div>
+            <a href="#">
+              <Button> CONTATO </Button>
+            </a>
+          </div>
+        </MenuDesktop>
 
-        <div>
-          <a href="#">
-            <Button> CONTATO </Button>
-          </a>
-        </div>
+        <ContainerSidebar scrolled={scrolled}>
+          <FaBars
+            onClick={() => {
+              setMenuActive(true)
+            }}
+          />
+        </ContainerSidebar>
+
+        <MenuMobile open={menuActive}>
+          <GrFormClose
+            onClick={() => {
+              setMenuActive(false)
+            }}
+          />
+          <NavMenu>
+            <ul>
+              <li>
+                <a href="#home"> Home </a>
+              </li>
+              <li>
+                <a href="#about"> Sobre </a>
+              </li>
+              <li>
+                <a href="#budgets"> Orçamentos </a>
+              </li>
+              <li>
+                <a href="#destiny"> Destinos </a>
+              </li>
+            </ul>
+          </NavMenu>
+        </MenuMobile>
+
+        <OverlayMenu
+          open={menuActive}
+          onClick={() => {
+            setMenuActive(false)
+          }}
+        />
       </Content>
     </Container>
   )
